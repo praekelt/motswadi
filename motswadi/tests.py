a074e5869522e98694ceaac10237708e90259487
Shaun Sephton <shaunsephton@gmail.com>
2012-03-13 17:27:50 +0200

    use vumi test_base

--- motswadi/tests.py
+++ motswadi/tests.py
@@ -1,7 +1,13 @@
-from unittest import TestCase
+from motswadi.workers import USSDApplicationWorker
+from vumi.application.tests.test_base import ApplicationTestCase
 
 
-class USSDApplicationWorkerTestCase(TestCase):
+class USSDApplicationWorkerTestCase(ApplicationTestCase):
+    application_class = USSDApplicationWorker
+
+    def setUp(self):
+        super(USSDApplicationWorkerTestCase, self).setUp()
+        self.app = self.get_application({}).result
 
     def test_consume_user_message(self):
-        import pdb; pdb.set_trace()
+        pass
