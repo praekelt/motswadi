4b1ec6bf3793d9a9549f91e1d6f29a0f4ade7036
shaunsephton <shaunsephton@gmail.com>
2012-03-15 12:40:19 +0200

    added evnt and teacher

--- motswadi/admin.py
+++ motswadi/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from motswadi.models import Guardian, NonAttendance, School, Student, \
-        Subject, SubjectResult, Test, TestResult
+from motswadi.models import Event, Guardian, NonAttendance, School, Student, \
+        Subject, SubjectResult, Teacher, Test, TestResult
 
+admin.site.register(Event)
 admin.site.register(Guardian)
 admin.site.register(NonAttendance)
 admin.site.register(School)
 admin.site.register(Student)
 admin.site.register(Subject)
 admin.site.register(SubjectResult)
+admin.site.register(Teacher)
 admin.site.register(Test)
 admin.site.register(TestResult)
