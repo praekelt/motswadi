269d7b7c078b1ab1ccae51a60c9daf422023e0e4
shaunsephton <shaunsephton@gmail.com>
2012-03-22 14:24:29 +0200

    more explicit db name

--- motswadi/settings_dev.py
+++ motswadi/settings_dev.py
@@ -4,6 +4,7 @@ DEBUG = True
 CREATE_DEFAULT_SUPERUSER = True
 
 DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+DATABASES['default']['NAME'] = 'motswadi.sqlite'
 
 INSTALLED_APPS = INSTALLED_APPS + (
     'snippetscream',
