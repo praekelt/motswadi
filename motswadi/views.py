2189f7db86fc00949646b8a152ed0c9aeaf3b405
shaunsephton <shaunsephton@gmail.com>
2012-03-22 14:42:37 +0200

    first commit

---
+++ motswadi/views.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
+
+
+@login_required
+def home(request):
+    return render_to_response("motswadi/home.html", {})
