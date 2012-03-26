eef23c0604bc92130d8ae7c2a7e125bb7ac91f16
shaunsephton <shaunsephton@gmail.com>
2012-03-26 09:42:47 +0200

    implemented Assessment create and update views

--- motswadi/views.py
+++ motswadi/views.py
@@ -2,20 +2,53 @@ from datetime import date
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, FormView
-from motswadi.models import NonAttendance, Teacher, Test
-from motswadi.forms import RollCallForm, TestForm
+from django.views.generic import CreateView, FormView, UpdateView
+from motswadi.models import AssessmentResult, NonAttendance, Teacher
+from motswadi.forms import RollCallForm
 
 
-class CreateTestView(CreateView):
-    model = Test
-    form_class = TestForm
+class CreateAssessmentResultView(CreateView):
+    model = AssessmentResult
+    success_url = '/'
 
-    def get_form_kwargs(self):
-        kwargs = super(CreateTestView, self).get_form_kwargs()
-        kwargs['user'] = Teacher.objects.get(pk=self.request.user.pk)
-        return kwargs
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(CreateAssessmentResultView, self).\
+                dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Thank you. Assessment result created.')
+        return super(CreateAssessmentResultView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        if '__all__' in form.errors:
+            if 'already exists' in ''.join(form.errors['__all__']):
+                pk = AssessmentResult.objects.get(
+                    title=form.data['title'],
+                    subject=form.data['subject'],
+                    student=form.data['student']
+                ).pk
+                form.errors['__all__'].append('<a href="%s">Update the "\
+                        "existing assessment result here.</a>' % \
+                        reverse("update_assessment_result", kwargs={'pk': pk}))
+        return super(CreateAssessmentResultView, self).form_invalid(form)
+
+
+class UpdateAssessmentResultView(UpdateView):
+    model = AssessmentResult
+    template_name = 'motswadi/assessmentresult_update_form.html'
+    success_url = '/'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(UpdateAssessmentResultView, self).\
+                dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Thank you. Assessment result updated.')
+        return super(UpdateAssessmentResultView, self).form_valid(form)
 
 
 class RollCallView(FormView):
