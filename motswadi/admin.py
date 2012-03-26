19a67a8d27a77abfa38b68eb673217371ae522f8
shaunsephton <shaunsephton@gmail.com>
2012-03-26 09:45:57 +0200

    cleanup

--- motswadi/admin.py
+++ motswadi/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from motswadi.models import Event, Guardian, NonAttendance, School, Student, \
-        Subject, SubjectResult, Teacher, Test, TestResult
+from motswadi.models import AssessmentResult, School, Student, Subject, Teacher
 
-admin.site.register(Event)
-admin.site.register(Guardian)
-admin.site.register(NonAttendance)
+admin.site.register(AssessmentResult)
 admin.site.register(School)
 admin.site.register(Student)
 admin.site.register(Subject)
-admin.site.register(SubjectResult)
 admin.site.register(Teacher)
-admin.site.register(Test)
-admin.site.register(TestResult)
