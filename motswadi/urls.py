13bf4946866ed4cb85de6162cec61ee417e8f189
shaunsephton <shaunsephton@gmail.com>
2012-03-26 09:43:08 +0200

    implemented Assessment create and update views

--- motswadi/urls.py
+++ motswadi/urls.py
@@ -1,18 +1,42 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
-from django.views.generic import CreateView, TemplateView
-from motswadi.models import TestResult
-from motswadi.views import CreateTestView, RollCallView
+from django.views.generic import TemplateView
+from motswadi.views import CreateAssessmentResultView, RollCallView, \
+        UpdateAssessmentResultView
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', login_required(TemplateView.as_view(template_name="motswadi/home.html")), name='home'),
-    url(r'^create/test$', login_required(CreateTestView.as_view()), name='create_test'),
-    url(r'^create/test-result$', login_required(CreateView.as_view(model=TestResult)), name='create_test_result'),
-    url(r'^roll-call$', RollCallView.as_view(), name='roll_call'),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(
+        r'^$',
+        login_required(TemplateView.as_view(
+            template_name="motswadi/home.html")
+        ),
+        name='home'
+    ),
+    url(
+        r'^admin/',
+        include(admin.site.urls)
+    ),
+    url(
+        r'^accounts/',
+        include('django.contrib.auth.urls')
+    ),
+    url(
+        r'^create-assessment-result$',
+        CreateAssessmentResultView.as_view(),
+        name='create_assessment_result'
+    ),
+    url(
+        r'^roll-call$',
+        RollCallView.as_view(),
+        name='roll_call'
+    ),
+    url(
+        r'^update-assessment-result/(?P<pk>\d+)$',
+        UpdateAssessmentResultView.as_view(),
+        name='update_assessment_result'
+    ),
 )
