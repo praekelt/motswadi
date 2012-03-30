8398e0c96bd34288d0020e4e5066bd639da42bc0
shaunsephton <shaunsephton@gmail.com>
2012-03-30 08:31:27 +0200

    corrected database engine

--- motswadi/settings.py
+++ motswadi/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'motswadi',                      # Or path to database file if using sqlite3.
         'USER': 'motswadi',                      # Not used with sqlite3.
         'PASSWORD': 'motswadi',                  # Not used with sqlite3.
