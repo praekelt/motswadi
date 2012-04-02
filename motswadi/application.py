af757192409b7977ca3b71ae4e084b5adec3adda
shaunsephton <shaunsephton@gmail.com>
2012-04-02 13:02:50 +0200

    menus updated to correlated with ussd designs

--- motswadi/application.py
+++ motswadi/application.py
@@ -1,9 +1,11 @@
+from datetime import date, timedelta
+
 # Initialize Django ENV.
 from django.core.management import setup_environ
-from motswadi import settings
-setup_environ(settings)
+from motswadi import settings_dev
+setup_environ(settings_dev)
 
-from motswadi.models import Student, Teacher
+from motswadi.models import Student, Subject
 from worker import DynamicMenuApplicationWorker, DynamicMenu
 
 
@@ -12,13 +14,20 @@ class AttendanceMenu(DynamicMenu):
     Resnpnds with attendance history for given student.
     """
     options = [
-        ('Back', {}, 'StudentMenu')
+        ('Back', {}, 'motswadi.application.StudentMenu'),
+        ('Main Menu', {}, 'motswadi.application.WelcomeMenu')
     ]
 
     def get_text(self):
         student = Student.objects.get(pk=self.session['student_pk'])
-        return "Attendance record for %s:\nAbsent - %s days." % \
-                (student.name, student.nonattendance_set.count())
+        absent_today = bool(student.nonattendance_set.filter(\
+                date=date.today()).count())
+        absent_count = student.nonattendance_set.filter(\
+                date__gte=date.today() - timedelta(days=30)).count()
+        return "-Attendance-\n%s is %sat school today.\n%s has been absent '\
+                '%s day%s out of the last 30 days." % \
+                (student, "not " if absent_today else "", student, \
+                absent_count, "" if absent_count == 1 else "s")
 
 
 class DatesMenu(DynamicMenu):
@@ -26,64 +35,49 @@ class DatesMenu(DynamicMenu):
     Responds with important dates for given student.
     """
     options = [
-        ('Back', {}, 'StudentMenu')
+        ('Back', {}, 'motswadi.application.StudentMenu'),
+        ('Main Menu', {}, 'motswadi.application.WelcomeMenu')
     ]
 
     def get_text(self):
         student = Student.objects.get(pk=self.session['student_pk'])
-        return "Important dates for %s:\n%s" % (student.name, '\n'.join(\
-                ['%s - %s' % (event.title, event.date) \
+        return "-Important dates-\n%s" % ('\n'.join(['%s: %s' % \
+                (event.date.strftime("%d %b"), event.title) \
                 for event in student.event_set.all()]))
 
 
 class TeacherInfoMenu(DynamicMenu):
     """
-    Responds with information about given teacher.
+    Responds with information about student class teacher.
     """
     options = [
-        ('Back', {}, 'TeacherMenu')
-    ]
-
-    def get_text(self):
-        teacher = Teacher.objects.get(pk=self.session['teacher_pk'])
-        return "Information for %s:\nContact Number - %s" % \
-                (teacher.name, teacher.contact_number)
-
-
-class TeacherMenu(DynamicMenu):
-    """
-    Menu listing various teachers for given student.
-    """
-    options = [
-        ('Back', {}, 'StudentMenu')
+        ('Back', {}, 'motswadi.application.StudentMenu'),
+        ('Main Menu', {}, 'motswadi.application.WelcomeMenu')
     ]
 
     def get_text(self):
         student = Student.objects.get(pk=self.session['student_pk'])
-        return "Teacher information for %s:" % student
-
-    def get_options(self):
-        teachers = Student.objects.get(pk=self.session['student_pk']).\
-                teacher_set.all()
-        return [('%s - %s' % (teacher.subject.title, teacher.name), \
-                {'teacher_pk': teacher.pk}, 'TeacherInfoMenu') \
-                for idx, teacher in enumerate(teachers)] + self.options
+        teacher = student.class_teacher
+        return '-Teacher Information-\n%s\nContact Number: %s\nPlease feel '\
+                'free to contact.' % (teacher, teacher.contact_number)
 
 
-class TestResultsMenu(DynamicMenu):
+class SubjectProgressMenu(DynamicMenu):
     """
-    Responds with latest test results for given student.
+    Responds with subject progress results for given student.
     """
     options = [
-        ('Back', {}, 'StudentMenu')
+        ('Back', {}, 'motswadi.application.AcademicProgressMenu'),
+        ('Main Menu', {}, 'motswadi.application.WelcomeMenu')
     ]
 
     def get_text(self):
         student = Student.objects.get(pk=self.session['student_pk'])
-        return "Latest tests results for %s:\n%s" % (student.name, '\n'.join(\
-                ['%s - %s (%s) - %s%%' % (result.test.subject.title, \
-                result.test.title, result.test.date, result.percentage) \
-                for result in student.testresult_set.all()[:5]]))
+        subject = Subject.objects.get(pk=self.session['subject_pk'])
+        return "-Academic progress-\n%s results for %s.\n%s" % \
+                (subject, student, '\n'.join(['%s: %s%%' % \
+                (result.title, result.percentage) for result in \
+                student.assessmentresult_set.filter(subject=subject)]))
 
 
 class StudentMenu(DynamicMenu):
@@ -91,32 +85,37 @@ class StudentMenu(DynamicMenu):
     Menu listing various report options for given student.
     """
     options = [
-        ('Attendance.', {}, 'AttendanceMenu'),
-        ('Subject marks.', {}, 'SubjectMarksMenu'),
-        ('Test results.', {}, 'TestResultsMenu'),
-        ('Important dates.', {}, 'DatesMenu'),
-        ('Teacher information.', {}, 'TeacherMenu'),
-        ('Back', {}, 'WelcomeMenu')
+        ('Attendance.', {}, 'motswadi.application.AttendanceMenu'),
+        ('Academic progress.', {}, \
+                'motswadi.application.AcademicProgressMenu'),
+        ('Important dates.', {}, 'motswadi.application.DatesMenu'),
+        ('Teacher information.', {}, 'motswadi.application.TeacherInfoMenu'),
+        ('Back', {}, 'motswadi.application.WelcomeMenu')
     ]
-    
+
     def get_text(self):
         student = Student.objects.get(pk=self.session['student_pk'])
-        return "Report choices for %s." % student
+        return "What would you like to view for %s?" % student
 
 
-class SubjectMarksMenu(DynamicMenu):
+class AcademicProgressMenu(DynamicMenu):
     """
     Responds with subject marks for given student.
     """
     options = [
-        ('Back', {}, 'StudentMenu')
+        ('Back', {}, 'motswadi.application.StudentMenu'),
+        ('Main Menu', {}, 'motswadi.application.WelcomeMenu')
     ]
 
     def get_text(self):
+        return "-Academic progress-\nWhich subject?"
+
+    def get_options(self):
         student = Student.objects.get(pk=self.session['student_pk'])
-        return "Subject marks for %s:\n%s" % (student.name, '\n'.join(\
-                ['%s - %s%%' % (result.subject.title, result.percentage) \
-                for result in student.subjectresult_set.all()]))
+        return [(subject.title, {'subject_pk': subject.pk}, \
+                'motswadi.application.SubjectProgressMenu') \
+                for idx, subject in enumerate(student.get_subjects())] \
+                + self.options
 
 
 class WelcomeMenu(DynamicMenu):
@@ -127,7 +126,8 @@ class WelcomeMenu(DynamicMenu):
 
     def get_options(self):
         students = Student.objects.all()
-        return [(student.name, {'student_pk': student.pk}, 'StudentMenu') \
+        return [(student.full_name, {'student_pk': student.pk}, \
+                'motswadi.application.StudentMenu') \
                 for idx, student in enumerate(students)]
 
 
