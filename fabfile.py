7113759660cca2500fd2436d6710f1ec28fa1bd5
shaunsephton <shaunsephton@gmail.com>
2012-04-02 08:43:31 +0200

    celanup before restart

--- fabfile.py
+++ fabfile.py
@@ -7,6 +7,7 @@ def restart():
 def provision():
     sudo('apt-get update')
     sudo('apt-get install -y git-core puppet')
+    run('rm -rf ~/motswadi')
     run('git clone https://github.com/praekelt/motswadi.git')
     with cd('motswadi'):
         sudo('puppet ./manifests/motswadi.pp --modulepath ./manifests/modules')
